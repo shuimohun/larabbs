da87bc81d72e54065e4a4ceaf4686928be54f3ac
shuimohun <8733517@qq.com>
2020-08-20 02:01:54 +0000

    填充用户和话题数据

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // 生成数据集合
+        $users = factory(User::class)->times(10)->create();
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'Shuimohun';
+        $user->email = '8733517@qq.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+    }
+}
