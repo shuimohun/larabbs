5d5c6a0c9ff77d44262ca94a912f7c8b8b522fcd
= <=>
2020-08-25 09:16:58 +0000

    集成 laravel-permission 扩展包

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,6 +16,11 @@ class UsersTableSeeder extends Seeder
         $user->email = '8733517@qq.com';
         $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->password = Hash::make('admin123');
+        $user->assignRole('Founder');
         $user->save();
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
